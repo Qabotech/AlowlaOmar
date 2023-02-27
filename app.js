3caef613274ad3107b25952ec2509150ef8094a7
Qabotech <81534574+Qabotech@users.noreply.github.com>
2023-02-27 09:00:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 $(".flex .box").append("<div class='overlay'></div>");
+function buttonSize() {
+    var width = $("#name").innerWidth();
+var height = $("#name").innerHeight();
+$('#send').innerWidth(width)
+$('#send').innerHeight(height)
+}
+buttonSize();
+$( window ).resize(function() {
+    buttonSize();
+  });
+
